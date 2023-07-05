3f526bf987c2c5ed5286b2b555dea5ca4eaec937
2Aram1Hakobyan2 <ar2hakobyan1@yahoo.com>
2023-07-05 16:56:07 +0400

    done

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        boolean greatest1 = first > second;
+        if (greatest1) {
+            System.out.println(first);
+        } else if (!greatest1) {
+            System.out.println(second);
+        } else {
+            System.out.println(first);
+        }
     }
 }
